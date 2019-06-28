6fe4fc66bae64a7e544cb6847ba2496d79bffc2e
Kapil Kumar <sangamsetia09@gmail.com>
2019-06-28 08:10:40 +0530

    Extracted out TableHeader from Movies Table to make it common

--- src/components/MoviesTable.js
+++ src/components/MoviesTable.js
@@ -1,33 +1,26 @@
 import React, { Component } from "react";
 import Like from "../common/Like";
+import TableHeader from "../common/TableHeader";
 
 class MoviesTable extends Component {
-  raiseSort(column) {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.column === column) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.column = column;
-      sortColumn.order = "asc";
-    }
-
-    this.props.onSort(sortColumn) ;
-  }
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    { key: "like" },
+    { key: "delete" }
+  ];
 
   render() {
-    const { movies, onDelete, onLike, onSort } = this.props;
+    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => this.raiseSort("title")}>Title</th>
-            <th onClick={() => this.raiseSort("genre.name")}>Genre</th>
-            <th onClick={() => this.raiseSort("numberInStock")}>Stock</th>
-            <th onClick={() => this.raiseSort("dailyRentalRate")}>Rate</th>
-            <th> </th>
-            <th> </th>
-          </tr>
-        </thead>
+        <TableHeader
+          columns={this.columns}
+          onSort={onSort}
+          sortColumn={sortColumn}
+        />
         <tbody>
           {movies.map(movie => (
             <tr key={movie._id}>
