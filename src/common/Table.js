6e2d20b82314370ba8e34a806f894fa7ae734aee
Kapil Kumar <sangamsetia09@gmail.com>
2019-06-30 15:16:39 +0530

    Added sort icon and applied a cursor style to it

--- src/common/Table.js
+++ src/common/Table.js
@@ -1,15 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import TableHeader from "./TableHeader";
 import TableBody from "./TableBody";
 
 const Table = ({ columns, data, onSort, sortColumn }) => {
   return (
     <table className="table">
-      <TableHeader
-        columns={columns}
-        onSort={onSort}
-        sortColumn={sortColumn}
-      />
+      <TableHeader columns={columns} onSort={onSort} sortColumn={sortColumn} />
 
       <TableBody data={data} columns={columns} />
     </table>
--- src/common/TableHeader.js
+++ src/common/TableHeader.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
 
 class TableHeader extends Component {
-    raiseSort(path) {
-        const sortColumn = { ...this.props.sortColumn };
-        if (sortColumn.column === path) {
-          sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-        } else {
-          sortColumn.column = path;
-          sortColumn.order = "asc";
-        }
-    
-        this.props.onSort(sortColumn);
-      }
+  raiseSort(path) {
+    const sortColumn = { ...this.props.sortColumn };
+    if (sortColumn.column === path) {
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    } else {
+      sortColumn.column = path;
+      sortColumn.order = "asc";
+    }
 
+    this.props.onSort(sortColumn);
+  }
+
+  renderSortIcon = column => {
+    const { column: path, order } = this.props.sortColumn;
+    if (column.path !== path) return null;
+    if (order === "asc") return <i className="fa fa-sort-asc" />;
+    return <i className="fa fa-sort-desc" />;
+  };
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map(column => (
-            <th onClick={() => this.raiseSort(column.path)}>{column.label}</th>
+            <th className='clickable' onClick={() => this.raiseSort(column.path)}>
+              {column.label} {this.renderSortIcon(column)}
+            </th>
           ))}
         </tr>
       </thead>
